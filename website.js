5ebf97b876ddbe74ea4e5090c0d9c1f2aece77a8
Richard Anaya <richard.anaya@gmail.com>
2011-09-02 17:22:48 -0500

    showing static file

--- website.js
+++ website.js
@@ -3,21 +3,15 @@ var express = require('express');
 var port = process.env.PORT || 9999;
 var server = express.createServer();
 
-server.configure( 
+server.configure(
     function() {
-	server.use(express.static(__dirname + '/root/'));
+        server.use(express.static(__dirname + '/root/'));
     }
 );
 
-server.get('/index.html', 
-    function(req,res) {
-	res.send('Hello World!');
-    }
-); 
-
-server.get(/^.*$/, 
+server.get(/^.*$/,
     function(req, res) {
-	res.redirect('/index.html');
+        res.redirect('/index.html');
     }
 );
 
