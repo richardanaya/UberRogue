57b76982452ee1c32298403519bcc77be1c08c38
Richard <richard.anaya@gmail.com>
2011-09-03 16:12:49 -0500

    facebookifying

--- website.js
+++ website.js
@@ -9,6 +9,12 @@ server.configure(
     }
 );
 
+server.post('/index.html',
+    function(req, res) {
+        res.redirect('https://www.facebook.com/dialog/oauth?client_id=271155132913470&redirect_uri=http://uberrogue.com/index.html');
+    }
+);
+
 server.get(/^.*$/,
     function(req, res) {
         res.redirect('/index.html');
@@ -22,12 +28,11 @@ io.enable('browser client minification');  // send minified client
 io.enable('browser client etag');          // apply etag caching logic based on version number
 io.set('log level', 1);                    // reduce logging
 io.set('transports', [                     // enable all transports (optional if you want flashsocket)
-//    'websocket'
-//  ,
-    'flashsocket'
-  , 'htmlfile'
-  , 'xhr-polling'
-  , 'jsonp-polling'
+//    'websocket',   //Chrome 13+ sems to be having problems with websocket =/
+    'flashsocket',
+    'htmlfile',
+    'xhr-polling',
+    'jsonp-polling'
 ]);
 var x = 5;
 var y = 5;
@@ -35,16 +40,16 @@ io.sockets.on('connection', function (socket) {
     socket.emit('drawCharacter', { x: x, y:y, symbol:'@'});
     socket.on('keyPressed', function(data) {
 
-        if(data == 87) {
+        if (data == 87) {
             y--;
         }
-        if(data == 83) {
+        if (data == 83) {
             y++;
         }
-        if(data == 65) {
+        if (data == 65) {
             x--;
         }
-        if(data == 68) {
+        if (data == 68) {
             x++;
         }
 
